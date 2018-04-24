290fe5683c419c808a0dc3cf3576062b53889131
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-25 01:57:56 +0800

    feat: Move the interface out to platfor and comment out all the interface as much as possible in order to allow it to be easy to test out the List Projects interface
    See also: #6

--- generic/platform.go
+++ generic/platform.go
@@ -1,22 +1,17 @@
 package generic
 
-// Platform is a generic struct to describe a platform being used to manage tasks
-type Platform struct {
-	Name string
-}
-
 // PlatformActions lists all actions that should be provided by a task management integration
 // In the case that the value is not meant to be available, we can think of providing defaults that would indicate so
 type PlatformActions interface {
 	ListProjects() ([]Project, error)
-	ListTasks(projectID string) ([]Task, error)
-	ListUsers(projectID string) ([]User, error)
-	CreateTask(projectID string) (Task, error)
-	CompleteTask(projectID, taskID string) (Task, error)
-	AssignTask(projectID, taskID, userID string) (Task, error)
-	AssignLabel(projectID, taskID, label string) (Task, error)
+	// ListTasks(projectID string) ([]Task, error)
+	// ListUsers(projectID string) ([]User, error)
+	// CreateTask(projectID string) (Task, error)
+	// CompleteTask(projectID, taskID string) (Task, error)
+	// AssignTask(projectID, taskID, userID string) (Task, error)
+	// AssignLabel(projectID, taskID, label string) (Task, error)
 }
 
-func (p Platform) ListProjects(actions PlatformActions) ([]Project, error) {
+func ListProjects(actions PlatformActions) ([]Project, error) {
 	return actions.ListProjects()
 }
--- generic/tasks.go
+++ generic/tasks.go
@@ -22,15 +22,3 @@ type Task struct {
 	Status      string
 	Subscribers []User
 }
-
-// PlatformActions lists all actions that should be provided by a task management integration
-// In the case that the value is not meant to be available, we can think of providing defaults that would indicate so
-type PlatformActions interface {
-	ListProjects() ([]Project, error)
-	ListTasks(projectID string) ([]Task, error)
-	ListUsers(projectID string) ([]User, error)
-	CreateTask(projectID string) (Task, error)
-	CompleteTask(projectID, taskID string) (Task, error)
-	AssignTask(projectID, taskID, userID string) (Task, error)
-	AssignLabel(projectID, taskID, label string) (Task, error)
-}
