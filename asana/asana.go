790c9c85703c72bc1b584da17f85015f0e64a3f8
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-25 02:01:28 +0800

    feat: Add a Asana struct that would mirror the interface required to fulfil generic.PlatformActions interface
    See also: #6

--- asana/asana.go
+++ asana/asana.go
@@ -1,4 +1,16 @@
 // Package asana provides the functionality to access the asana API to manipulate and obtain the list from the tool
 package asana
 
+import "github.com/hairizuanbinnoorazman/tasker/generic"
+
 var asanaURL = "https://app.asana.com/api/1.0/"
+
+type Asana struct {
+	Token string
+}
+
+func (a Asana) ListProjects() ([]generic.Project, error) {
+	token := a.Token
+	err := ListProjects(token)
+	return []generic.Project{}, err
+}
