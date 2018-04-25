96e084cff93d764a5834afa896b46420bdd6b9cb
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-26 00:45:32 +0800

    docs: Add documentation for each of the generic functions that needs to be implemented by each of the platforms

--- generic/platform.go
+++ generic/platform.go
@@ -12,30 +12,37 @@ type PlatformActions interface {
 	AssignLabel(projectID, taskID, label string) (Task, error)
 }
 
+// ListProjects is a generic function that runs the platform's list project command
 func ListProjects(actions PlatformActions) ([]Project, error) {
 	return actions.ListProjects()
 }
 
+// ListTasks is a generic function that runs the platform's list tasks command
 func ListTasks(actions PlatformActions, projectID string) ([]Task, error) {
 	return actions.ListTasks(projectID)
 }
 
+// ListUsers is a generic function that runs the platform's list users command based of Project
 func ListUsers(actions PlatformActions, projectID string) ([]User, error) {
 	return actions.ListUsers(projectID)
 }
 
+// CreateTask is a generic function that runs the platform's create task command
 func CreateTask(actions PlatformActions, projectID, taskName string) (Task, error) {
 	return actions.CreateTask(projectID, taskName)
 }
 
+// CompleteTask is a generic function that runs the platform's complete task command
 func CompleteTask(actions PlatformActions, projectID, taskID string) (Task, error) {
 	return actions.CompleteTask(projectID, taskID)
 }
 
+// AssignTask is a generic function that runs the platform's assign task command
 func AssignTask(actions PlatformActions, projectID, taskID, userID string) (Task, error) {
 	return actions.AssignTask(projectID, taskID, userID)
 }
 
+// AssignLabel is a generic function that runs the platform's assign label command
 func AssignLabel(actions PlatformActions, projectID, taskID, label string) (Task, error) {
 	return actions.AssignLabel(projectID, taskID, label)
 }
