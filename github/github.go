dc2814acbaf0aae3ba0dd707c17d36c19803084f
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-26 00:56:56 +0800

    feat: Add the initial skeleton for the github project concrete implementation

---
+++ github/github.go
@@ -0,0 +1,34 @@
+// Package asana provides the functionality to access the asana API to manipulate and obtain the list from the tool
+package asana
+
+import "github.com/hairizuanbinnoorazman/tasker/generic"
+
+type Github struct{}
+
+func (a Github) ListProjects() ([]generic.Project, error) {
+	return []generic.Project{}, nil
+}
+
+func (a Github) ListTasks(projectID string) ([]generic.Task, error) {
+	return []generic.Task{}, nil
+}
+
+func (a Github) CreateTask(projectID, name string) (generic.Task, error) {
+	return generic.Task{}, nil
+}
+
+func (a Github) ListUsers(projectID string) ([]generic.User, error) {
+	return []generic.User{}, nil
+}
+
+func (a Github) CompleteTask(projectID, taskID string) (generic.Task, error) {
+	return generic.Task{}, nil
+}
+
+func (a Github) AssignTask(projectID, taskID, userID string) (generic.Task, error) {
+	return generic.Task{}, nil
+}
+
+func (a Github) AssignLabel(projectID, taskID, label string) (generic.Task, error) {
+	return generic.Task{}, nil
+}
