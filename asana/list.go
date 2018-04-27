0a012d62d3a04e97813d0de483b28477eef962b6
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-28 02:28:44 +0800

    refactor: Move list users to its own separate user file

--- asana/list.go
+++ asana/list.go
@@ -35,10 +35,3 @@ func listTasks(token, project string) error {
 	fmt.Println(string(value))
 	return nil
 }
-
-// listUsers function in Asana requires a 2 step process to retrive users
-// Part 1: Obtain the team ID
-// Part 2: From the team ID, list all members within that group
-func listUsers(token, project string) error {
-	return nil
-}
---
+++ asana/users.go
@@ -0,0 +1,8 @@
+package asana
+
+// listUsers function in Asana requires a 2 step process to retrive users
+// Part 1: Obtain the team ID
+// Part 2: From the team ID, list all members within that group
+func listUsersByProject(token, project string) error {
+	return nil
+}
