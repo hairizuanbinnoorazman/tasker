38c095837dac4b5d91a40608e62a1b23f7396df4
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-28 02:26:21 +0800

    refactor: Move the list project code to the project file

--- asana/list.go
+++ asana/list.go
@@ -15,26 +15,6 @@ type genericItem struct {
 	Name string `json:"name"`
 }
 
-func listProjects(token string) error {
-	projectURL := asanaURL + "projects"
-	resp, err := genericHTTPGet(token, projectURL)
-	if err != nil {
-		fmt.Println("Error in getting response")
-		return err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	var parsedResponse genericList
-	errJSON := json.Unmarshal(body, &parsedResponse)
-	if errJSON != nil {
-		fmt.Println("Error in unmarshalling content")
-		return errJSON
-	}
-	value, _ := json.MarshalIndent(parsedResponse, "", "\t")
-	fmt.Println(string(value))
-	return nil
-}
-
 func listTasks(token, project string) error {
 	projectTaskURL := asanaURL + fmt.Sprintf("projects/%s/tasks", project)
 	resp, err := genericHTTPGet(token, projectTaskURL)
--- asana/project.go
+++ asana/project.go
@@ -33,3 +33,23 @@ func getProject(token, projectID string) (project, error) {
 	}
 	return parsedResponse.Data, nil
 }
+
+func listProjects(token string) error {
+	projectURL := asanaURL + "projects"
+	resp, err := genericHTTPGet(token, projectURL)
+	if err != nil {
+		fmt.Println("Error in getting response")
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	var parsedResponse genericList
+	errJSON := json.Unmarshal(body, &parsedResponse)
+	if errJSON != nil {
+		fmt.Println("Error in unmarshalling content")
+		return errJSON
+	}
+	value, _ := json.MarshalIndent(parsedResponse, "", "\t")
+	fmt.Println(string(value))
+	return nil
+}
