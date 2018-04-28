53f415b4ac7f33ceed349daeba6c4be771e56bde
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-29 01:17:03 +0800

    feat: Add a put helper which would help in sending requests more easily to the req helper

--- asana/reqHelper.go
+++ asana/reqHelper.go
@@ -1,12 +1,28 @@
 package asana
 
 import (
+	"io"
 	"net/http"
 )
 
 func genericHTTPGet(token, url string) (*http.Response, error) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func genericHTTPPut(token, url string, body io.Reader) (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("PUT", url, body)
 	if err != nil {
 		return nil, err
 	}
