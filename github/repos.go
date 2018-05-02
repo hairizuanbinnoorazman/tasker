3f73701a4631165d23df9060f368d392cba347c5
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-02 23:52:05 +0800

    feat: Add the generic http get method for extracting data from github

--- github/repos.go
+++ github/repos.go
@@ -1,3 +1,10 @@
 package github
 
-func listRepos(token string) error {}
+func listRepos(token string) error {
+	repoURL := githubURL + "/user/repos"
+	resp, err := genericHTTPGet(token, repoURL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
