ae352e6a1e1764f3ef92d5ae7870538631544b52
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-03 13:50:33 +0800

    docs: Updated the documentation for hte list projects and list tasks public github service

--- github/github.go
+++ github/github.go
@@ -5,12 +5,22 @@ import "github.com/hairizuanbinnoorazman/tasker/generic"
 
 var githubURL = "https://api.github.com"
 
-type Service struct{}
+// Service struct defines the data tokens required to allow the data to be pulled out
+// tokens are not tied to the function signatures as there is no guarantee that the
+// different task management platforms utilize the same way of authenticating with the same
+// kind of values
+type Service struct {
+	token string
+}
 
+// ListProjects in the context of the Github tasker package revolves around the concept of repos
+// This command would list the repos that users would be able to access to
 func (a Service) ListProjects() ([]generic.Project, error) {
 	return []generic.Project{}, nil
 }
 
+// ListTasks in the context of the Githb tasker package revolves around the concept ofn issues
+// This command would list down list of issues of the repo.
 func (a Service) ListTasks(projectID string) ([]generic.Task, error) {
 	return []generic.Task{}, nil
 }
