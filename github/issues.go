c3ac77ff6ec8b4eaab4892c8c648d42bd8bd2d09
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-03 17:58:28 +0800

    chore: Initial portion of implementation for github issues

--- github/issues.go
+++ github/issues.go
@@ -1,5 +1,18 @@
 package github
 
-func listIssues(token string) error {
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func listIssues(token, projectID string) error {
+	issueURL := githubURL + fmt.Sprintf("/repos/%v/issues", projectID)
+	resp, err := genericHTTPGet(token, issueURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
 	return nil
 }
